df6db7d97738fe5516e4c9c69c0a3d4fb09eb336
Miloovann <62134704+Miloovann@users.noreply.github.com>
2021-03-02 14:45:51 +0800

    c

--- HeaderSelector.mjs
+++ HeaderSelector.mjs
@@ -1,4 +1,4 @@
-var continents = [['int', 'Global'],['singapore', 'Singapore'],['afr', 'Africa'],['asa', 'Asia'],['aus', 'Australia/Oceania'],['eu', 'Europe'],['nam', 'North America'], ['sam', 'South America']];
+var continents = [['int', 'Global'],['singapore', 'Singapore'],['afr', 'Africa'],['asa', 'Asia'],['aus', 'Oceania'],['eu', 'Europe'],['nam', 'North America'], ['sam', 'South America']];
 var filterfunctioncode = '';
 continents.forEach(cont=>{
     filterfunctioncode += '<li class="nav-item"><p class="hover nav-item nav-link" onclick="filter(\'' + cont[0] + '\')" style="font-size:25px; color:white">' + cont[1] + '</p></li>';
