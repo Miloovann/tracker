f5a9af9857429970cf4e29b80932300abc172042
Miloovann <62134704+Miloovann@users.noreply.github.com>
2021-03-04 19:37:42 +0800

    c

--- covidtables.mjs
+++ covidtables.mjs
@@ -66,7 +66,7 @@ function sgdatelink(){
         else if(month===11){month = 'Dec';}
 
         date = day+' '+month;
-        filename = 'https://covidhunter.netlify.app/; //rmb change url
+        filename = 'https://covidhunter.netlify.app/'; //rmb change url
         filename += 'SG\ ' + day+'\ '+month + '\ Cases.svg';
         console.log(filename);
         if(checkfileexists(filename)){
